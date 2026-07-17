9478b23575f1018faea4aadca5c992525f40e445
sana001-alt <www.allahforgiveness877@gmail.com>
2026-07-17 21:58:20 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <WebView 
+        source={{ uri: 'https://ais-pre-micgqszjsglkhqfgnzraz-572875732715.asia-southeast1.run.app' }} 
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
